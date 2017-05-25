be93ff75d10ec80d1fd0e53d275955de645d82f3
john.huffman@infielddigital.com <john.huffman@infielddigital.com>
2017-05-25 07:49:54 -0600

    updates to npm and adding a contact section to page

--- src/resources/javascripts/frontend/react/interests/Interests.jsx
+++ src/resources/javascripts/frontend/react/interests/Interests.jsx
@@ -1,21 +1,35 @@
 import React from 'react';
 
-export default class About extends React.Component {
+export default class Interests extends React.Component {
     render() {
         return (
-            <div className="w3-content w3-padding w3-center">
-                <h1>INTERESTS</h1>
-                <p className="w3-wide">Photography</p>
-                <div className="w3-light-grey">
-                    <div className="w3-container w3-center w3-padding-small w3-dark-grey" style={{width:'95%'}}>95%</div>
-                </div>
-                <p className="w3-wide">Web Design</p>
-                <div className="w3-light-grey">
-                    <div className="w3-container w3-center w3-padding-small w3-dark-grey" style={{width:'85%'}}>85%</div>
-                </div>
-                <p className="w3-wide">Photoshop</p>
-                <div className="w3-light-grey">
-                    <div className="w3-container w3-center w3-padding-small w3-dark-grey" style={{width:'80%'}}>80%</div>
+            <div className="w3-container w3-section w3-light-grey">
+                <div className="w3-padding w3-center">
+                    <h1>INTERESTS</h1>
+                    <p>Laravel</p>
+                    <div className="w3-light-grey">
+                        <div className="w3-container w3-center w3-padding-small w3-dark-grey" style={{width:'100%'}}>100%</div>
+                    </div>
+                    <p>Express/Sail/Meteor</p>
+                    <div className="w3-light-grey">
+                        <div className="w3-container w3-center w3-padding-small w3-dark-grey" style={{width:'90%'}}>90%</div>
+                    </div>
+                    <p>React/Angular/Vue/Ember</p>
+                    <div className="w3-light-grey">
+                        <div className="w3-container w3-center w3-padding-small w3-dark-grey" style={{width:'85%'}}>85%</div>
+                    </div>
+                    <p>Python Django/Flask</p>
+                    <div className="w3-light-grey">
+                        <div className="w3-container w3-center w3-padding-small w3-dark-grey" style={{width:'100%'}}>100%</div>
+                    </div>
+                    <p>Ruby Rails/Sinatra</p>
+                    <div className="w3-light-grey">
+                        <div className="w3-container w3-center w3-padding-small w3-dark-grey" style={{width:'70%'}}>70%</div>
+                    </div>
+                    <p>Golang</p>
+                    <div className="w3-light-grey">
+                        <div className="w3-container w3-center w3-padding-small w3-dark-grey" style={{width:'80%'}}>80%</div>
+                    </div>
                 </div>
             </div>
         );
