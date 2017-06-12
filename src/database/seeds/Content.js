cae260949ef426fafa2f3b4a4967622916ded657
John Huffman <j-huffman@hotmail.com>
2017-06-12 17:49:10 -0600

    adding update to content seed

--- src/database/seeds/Content.js
+++ src/database/seeds/Content.js
@@ -1,28 +1,34 @@
 const contents = {
     "about": [
         {
-            "key": "SectionOne",
+            "key": "AboutOne",
             "icon": "fa-heartbeat",
             "header": "Activination",
             "content": "I'm an Archer and practitioner of Aikido. I also like to fish, camp, play golf, and lift weights."
         },
         {
-            "key": "SectionTwo",
+            "key": "AboutTwo",
             "icon": "fa-rebel",
             "header": "Nerdificated",
             "content": "Talk to me about Star Wars, Dungeons and Dragons, Space, History... anything awesome really."
         },
         {
-            "key": "SectionThree",
+            "key": "AboutThree",
             "icon": "fa-linux",
             "header": "Nerdificated",
             "content": "I'm a big fan of animals. I have 2 cats but I like animals great and small."
         },
         {
-            "key": "SectionFour",
+            "key": "AboutFour",
             "icon": "fa-rocket",
             "header": "Technologicon",
             "content": "I have interest in the philosophy of futurism and transhumanism. Basically, I want a space ship and a robot arm."
+        }
+    ],
+    "interests": [
+        {
+            "key": "InterestOne",
+
         }
     ]
 };
